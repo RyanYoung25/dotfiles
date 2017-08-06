4442081869da0582c649ae49f297ab62a348edd5
Ryan <ryan@rdyoung.us>
2017-08-06 10:49:09 -0400

    Added other dot files and a helper script

---
+++ linkHelper.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#Make symlinks in the appropriate parts of the home directory
+
+CUR_DIR=pwd
+vim_link () {
+    #Setup vim
+
+    #Setup all of the necessary vim dirs that I have
+    mkdir -p ~/.vim/dirs/tmp
+    mkdir -p ~/.vim/dirs/backups
+    mkdir -p ~/.vim/dirs/undos
+    
+    #Link the vimrc to ~/.vimrc
+    ln -s $CUR_DIR/vimrc ~/.vimrc
+}
+
+tmux_link () {
+    #Setup tmux 
+
+    #Link the tmux.conf to ~/.tmux.conf
+    ln -s $CUR_DIR/tmux.conf ~/.tmux.conf
+}
+
+i3_link () {
+    #Setup i3 
+
+    #Link i3Config to ~/.i3/config
+    ln -s $CUR_DIR/i3Config ~/.i3/config
+    #Link i3status.conf to ~/.i3status.conf
+    ln -s $CUR_DIR/i3status.conf ~/.i3status.conf 
+}
+
+show_help () {
+    #Echo usage
+    echo "Usage: ./linkHelper.sh <option>"
+    echo "Options: "
+    echo "-v link vim"
+    echo "-t link tmux"
+    echo "-i link i3"
+    echo "-h show help"
+
+}
+
+
+if [ -z "$*" ]; 
+then 
+    show_help
+    exit 0
+fi
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+echo "Trying to read ops"
+while getopts hvti opt; do
+    case "$opt" in
+    h)
+        show_help
+        exit 0
+        ;;
+    v)  vim
+        ;;
+    t)  tmux_link 
+        ;;
+    i)  i3_link 
+        ;;
+    esac
+done
+exit 0
